7a5c96225eeef6e33aff8bacf161705d8b5be2d4
YUNJAEGOONER <coygyj@gmail.com>
2026-01-17 23:21:39 +0900

    docs: velog에서 "이걸 세야해?" 글 옮기기

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1768137345',resources: [
+    cacheName: 'chirpy-1768659542',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
