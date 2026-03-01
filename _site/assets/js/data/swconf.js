4750b3ed5e82b281c2a632a2323dbc39d97f1259
YUNJAEGOONER <coygyj@gmail.com>
2026-03-01 16:54:30 +0900

    docs: 무작정 따라하는 배포 시리즈 작성

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1770473382',resources: [
+    cacheName: 'chirpy-1772269463',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
