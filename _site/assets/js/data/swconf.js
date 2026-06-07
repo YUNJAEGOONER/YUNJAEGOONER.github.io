a41af553970373da65c5222b7339f9edef547968
YUNJAEGOONER <coygyj@gmail.com>
2026-06-07 17:21:42 +0900

    docs:cors 정리

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1775980931',resources: [
+    cacheName: 'chirpy-1780820488',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
